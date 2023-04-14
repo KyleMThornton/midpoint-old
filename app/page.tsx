8cdf827ec9154735783e083a97d914d6ab2554b9
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-04-14 11:04:55 -0700

    Slight layout changes. Updated README.

--- app/page.tsx
+++ app/page.tsx
@@ -19,13 +19,13 @@ export default function Home() {
       <p className="p-5">Set your location and a second location to find the point in the middle.</p>
       <div className="flex p-5">
         <div className="flex flex-col items-center">
-          <button className="rounded-full text-white bg-sky-500 p-5 hover:bg-sky-600 active:bg-sky-700 w-60" onClick={getMyLocation}>Get User Location</button>
-          {myLocationLat ? <h2>Lat: {myLocationLat}</h2> : null }
+          <button className="rounded-full text-white bg-sky-500 p-4 hover:bg-sky-600 active:bg-sky-700 w-60" onClick={getMyLocation}>Get User Location</button>
+          {myLocationLat ? <h2 className="mt-5">Lat: {myLocationLat}</h2> : null }
           {myLocationLon ? <h2>Lon: {myLocationLon}</h2> : null }
         </div>
         <div className="flex flex-col items-center">
-          <button className="rounded-full text-white bg-sky-500 p-5 hover:bg-sky-600 active:bg-sky-700 w-60">Get Second Location</button>
-          <input type="text" className="border-2 border-black border-solid rounded m-5 w-96" />
+          <button className="rounded-full text-white bg-sky-500 p-4 hover:bg-sky-600 active:bg-sky-700 w-60">Get Second Location</button>
+          <input type="text" className="border-2 border-black border-solid rounded m-5 w-60" />
         </div>
       </div>
     </main>
