0e6d32dc38b260a78bf75413ff55eb60fb607871
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-04-13 22:43:06 -0700

    Location and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
 "use client"
-
-async function getLocation() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position);
-  })
-}
-
+import { useState } from "react";
 
 export default function Home() {
+  const [myLocationLat, setMyLocationLat] = useState<number>();
+  const [myLocationLon, setMyLocationLon] = useState<number>();
+  const [theirLocation, setTheirLocation] = useState();
+
+  async function getLocation() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setMyLocationLat(position.coords.latitude);
+      setMyLocationLon(position.coords.longitude);
+    })
+  }
 
   return (
     <main className='flex flex-col items-center'>
       <h1 className="text-lg py-2 m-4 font-semibold">My first Next.JS app</h1>
       <p className="p-5">Welcome to my first Next.js app. I'm mostly just messing around as I try to learn Next.js, Tailwind CSS, and Typescript while also brushing up on React.</p>
       <button className="rounded-full text-white bg-sky-500 p-5 hover:bg-sky-600 active:bg-sky-700" onClick={getLocation}>Get User Location</button>
-      
+      {myLocationLat ? <h2>{myLocationLat} + {myLocationLon}</h2> : null }
+      <input type="text" className="border-2 border-black border-solid rounded m-5 w-96" />
+      <button className="rounded-full text-white bg-sky-500 p-5 hover:bg-sky-600 active:bg-sky-700">Set Second Location</button>
     </main>
   )
 }
