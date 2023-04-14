9d3fc46bdf297f6a9118bb279ebb2468a23d39d3
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-04-14 10:42:57 -0700

    Moved info to about, rearranged main layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,8 @@
 export default function About() {
     return (
-        <div className="flex justify-center">
+        <div className="flex flex-col justify-center items-center">
             <h1 className="text-lg py-2 m-4 font-semibold">About</h1>
+            <p className="p-5">Welcome to my first Next.js app. I'm mostly just messing around as I try to learn Next.js, Tailwind CSS, and Typescript while also brushing up on React.</p>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [myLocationLon, setMyLocationLon] = useState<number>();
   const [theirLocation, setTheirLocation] = useState();
 
-  async function getLocation() {
+  async function getMyLocation() {
     navigator.geolocation.getCurrentPosition(function(position) {
       setMyLocationLat(position.coords.latitude);
       setMyLocationLon(position.coords.longitude);
@@ -16,11 +16,18 @@ export default function Home() {
   return (
     <main className='flex flex-col items-center'>
       <h1 className="text-lg py-2 m-4 font-semibold">My first Next.JS app</h1>
-      <p className="p-5">Welcome to my first Next.js app. I'm mostly just messing around as I try to learn Next.js, Tailwind CSS, and Typescript while also brushing up on React.</p>
-      <button className="rounded-full text-white bg-sky-500 p-5 hover:bg-sky-600 active:bg-sky-700" onClick={getLocation}>Get User Location</button>
-      {myLocationLat ? <h2>{myLocationLat} + {myLocationLon}</h2> : null }
-      <input type="text" className="border-2 border-black border-solid rounded m-5 w-96" />
-      <button className="rounded-full text-white bg-sky-500 p-5 hover:bg-sky-600 active:bg-sky-700">Set Second Location</button>
+      <p className="p-5">Set your location and a second location to find the point in the middle.</p>
+      <div className="flex p-5">
+        <div className="flex flex-col items-center">
+          <button className="rounded-full text-white bg-sky-500 p-5 hover:bg-sky-600 active:bg-sky-700 w-60" onClick={getMyLocation}>Get User Location</button>
+          {myLocationLat ? <h2>Lat: {myLocationLat}</h2> : null }
+          {myLocationLon ? <h2>Lon: {myLocationLon}</h2> : null }
+        </div>
+        <div className="flex flex-col items-center">
+          <button className="rounded-full text-white bg-sky-500 p-5 hover:bg-sky-600 active:bg-sky-700 w-60">Get Second Location</button>
+          <input type="text" className="border-2 border-black border-solid rounded m-5 w-96" />
+        </div>
+      </div>
     </main>
   )
 }
